5a6668b7a9bf471ebe80a562038e3f4d27901fd5
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2023-10-10 15:46:01 +0300

    Works

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         style={{ backgroundSize: 'cover', backgroundPosition: '50%', backgroundRepeat: 'no-repeat', backgroundImage: `radial-gradient(circle at top left,rgba(40,40,40,0.79) 0%,rgba(40,40,40,0.86) 100%),url('/E1Ktausta.png')`}} 
         className="w-full bg-gray-300 px-5 sm:px-20  py-60 md:py-80 relative text-white flex flex-col justify-center items-center text-center">
         <h1 className=' relative z-10 text-4xl md:text-7xl font-bold py-2 md:py-6'>E1K Urakointi Oy</h1>
-        <h4 className=' relative z-10 text-lg md:text-2xl pb-10 max-w-[850px]'>Urakoitsija uusimaalla rakennusliikkeille, yrityksille ja yksityisille. Urakat valmiiksi tehokkaasti, nopealla aikataululla</h4>
+        <h4 className=' relative text-gray-300 z-10 text-lg md:text-2xl pb-10 max-w-[850px]'>Urakoitsija uusimaalla rakennusliikkeille, yrityksille ja yksityisille. Urakat valmiiksi tehokkaasti, nopealla aikataululla</h4>
         {/* <h6 className=' relative z-10 text-2xl'>Työllistämme ammattitaitoisia rakennushenkilöitä</h6> */}
       </div>
       <div className="w-full px-5 sm:px-20 py-10 md:py-48 relative" style={{filter: 'saturate(0%) brightness(94%) contrast(120%)', backfaceVisibility: 'hidden'}}>
@@ -54,7 +54,7 @@ export default function Home() {
       <div className="w-full bg-blue-100 px-5 sm:px-20 py-48 relative" style={{ backgroundSize: 'cover', backgroundPosition: '50%', backgroundRepeat: 'no-repeat', backgroundImage: `radial-gradient(circle at top left,rgba(40,40,40,0.79) 0%,rgba(40,40,40,0.86) 100%),url('/lumenajo.jpeg')`}}>
         <div className='relative z-10 text-white flex flex-col justify-center items-center py-10 text-center'>
           <h2 className=' font-bold text-4xl md:text-7xl py-2 md:py-6'>E1K Urakointi Oy</h2>
-          <h6 className=' text-lg md:text-2xl'>Luotettavaa ja ammattitaitoista osaamista</h6>
+          <h6 className=' text-lg text-gray-300 md:text-2xl'>Luotettavaa ja ammattitaitoista osaamista</h6>
           <button className='w-40 px-5 mt-8 py-3 gap-2 bg-orange-400 shadow-md text-text_secondary'>Galleriamme</button>
         </div>
       </div>
